3443dcd329d6915c2bb5e824a9c0a914f55a3042
Andy-Shao <hj13035952619@126.com>
2015-04-12 17:08:05 +0800

    add BuildXml

---
+++ BuildXml.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BuildXml {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Please Input:");
+        String input = BuildXml.readInfo(new BufferedReader(new InputStreamReader(System.in)) , "\\u0x04");
+        System.out.println(input.replaceAll("<" , "&lt;").replaceAll(">" , "&gt;"));
+        System.out.println("END!!");
+    }
+
+    static String readInfo(BufferedReader reader , String endSign) throws IOException {
+        StringBuffer buffer = new StringBuffer();
+        for (String temp = "" ; (temp = reader.readLine()) != null && temp.indexOf(endSign) == -1 ;)
+            buffer.append(temp).append("\n");
+        return buffer.substring(0 , buffer.length() - 1).toString();
+    }
+}
